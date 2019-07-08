0b872a38ed028aaa07e3191125e8107971ebe983
mohsenMahmoodzadeh <1mohsen.smm@gmail.com>
2019-07-08 20:20:15 +0430

    some comments added

---
+++ sync vs async.js
@@ -0,0 +1,10 @@
+/*
+setTimeout(arg1,arg2) has scheduled a task to be performed in the future(in this case 2000ms). 
+so this task(arg1) will be called after 2000ms and during this time, the rest of program is executing.  
+*/
+
+console.log('Before');
+setTimeout(() => {
+	console.log('Reading a user from a database... ');
+} , 2000);
+console.log('After')
